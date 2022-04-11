011fe2334a12a1281c8d94af0219fa577963632c
Maximilian Winter <maximilian.winter.91@gmail.com>
2022-04-11 23:08:20 +0200

    Swichted to IL2CPP

--- Assets/BiteUnity/BiteMoveGameObject.cs
+++ Assets/BiteUnity/BiteMoveGameObject.cs
@@ -6,12 +6,17 @@ using System.Linq;
 using Bite.Compiler;
 using Bite.Runtime.CodeGen;
 using UnityEngine;
+using UnityEngine.Scripting;
 
 public class BiteMoveGameObject : MonoBehaviour
 {
     [SerializeField]
     private GameObject m_GameObjectToMove;
     
+    [SerializeField]
+    private Transform m_TransformToMove;
+
+    public Vector3 m_Vector3;
     private BiteProgram program; 
     void Start()
     {
@@ -32,7 +37,8 @@ public class BiteMoveGameObject : MonoBehaviour
         program.Run(new Dictionary < string, object >()
         {
             { "GameObjectFromUnity", m_GameObjectToMove },
-            //{ "b", 2 }
+            { "TransformToUse", m_TransformToMove },
+            { "Vector3", m_Vector3 }
         });
     }
 }
