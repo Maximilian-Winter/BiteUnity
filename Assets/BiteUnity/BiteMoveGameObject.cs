c25289d45908eaa0c26f1072c4d69397dd23d104
Maximilian Winter <maximilian.winter.91@gmail.com>
2022-04-13 05:53:08 +0200

    Added rotate in circle example

--- Assets/BiteUnity/BiteMoveGameObject.cs
+++ Assets/BiteUnity/BiteMoveGameObject.cs
@@ -3,24 +3,49 @@ using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Threading;
+using System.Threading.Tasks;
 using Bite.Compiler;
+using Bite.Modules.Callables;
+using Bite.Runtime;
 using Bite.Runtime.CodeGen;
+using Bite.Runtime.Functions.ForeignInterface;
+using BiteUnity;
+using UnityEditor;
 using UnityEngine;
 using UnityEngine.Scripting;
 
+public class DeltaTimeGetter
+{
+    public float DeltaTime => Time.deltaTime;
+}
 public class BiteMoveGameObject : MonoBehaviour
 {
     [SerializeField]
     private GameObject m_GameObjectToMove;
     
     [SerializeField]
-    private Transform m_TransformToMove;
+    private Transform m_TransformTarget;
 
-    public Vector3 m_Vector3;
-    private BiteProgram program; 
+
+    private BiteProgram program;
+    private BiteVm vm = new BiteVm();
+
     void Start()
     {
-        IEnumerable < string > files = Directory.EnumerateFiles(
+        Debug.Log("Started BiteMoveGameObject!");
+        vm.InitVm();
+        vm.SynchronizationContext = SynchronizationContext.Current;
+      
+        vm.RegisterExternalGlobalObjects(new Dictionary<string, object>()
+        {
+            { "Camera", m_GameObjectToMove },
+            { "TransformTarget", m_TransformTarget },
+        });
+
+        EditorApplication.playModeStateChanged += EditorApplication_playModeStateChanged;
+
+        IEnumerable< string > files = Directory.EnumerateFiles(
             "Assets\\Bite\\TestMoveGameObject",
             "*.bite",
             SearchOption.AllDirectories );
@@ -28,17 +53,34 @@ public class BiteMoveGameObject : MonoBehaviour
         BiteCompiler compiler = new BiteCompiler();
 
         program = compiler.Compile(files.Select(File.ReadAllText));
-        
-       
+
+        program.TypeRegistry.RegisterType<Vector3>();
+        vm.RegisterSystemModuleCallables( program.TypeRegistry );
+        vm.RegisterCallable( "UnityDeltaTime", new UnityDeltaTimeVmCallable() );
+        Task.Run(() =>
+        {
+            Debug.Log("Running program");
+            vm.Interpret(program);
+        }).ContinueWith(t=>
+        {
+            Debug.Log("Stopped!");
+            if (t.IsFaulted)
+            {
+                Debug.LogError(t.Exception.InnerException.Message);
+            }
+        });       
+    }
+
+    private void EditorApplication_playModeStateChanged(PlayModeStateChange obj)
+    {
+        if(obj == PlayModeStateChange.ExitingPlayMode)
+        {
+            vm.Stop();
+        }
     }
 
     private void Update()
     {
-        program.Run(new Dictionary < string, object >()
-        {
-            { "GameObjectFromUnity", m_GameObjectToMove },
-            { "TransformToUse", m_TransformToMove },
-            { "Vector3", m_Vector3 }
-        });
+
     }
 }
---
+++ Assets/BiteUnity/UnityDeltaTimeVmCallable.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections.Generic;
+using Bite.Runtime.Functions;
+using Bite.Runtime.Memory;
+using UnityEngine;
+
+namespace BiteUnity
+{
+
+public class UnityDeltaTimeVmCallable: IBiteVmCallable
+{
+    public object Call( List < DynamicBiteVariable > arguments )
+    {
+        return Time.deltaTime;
+    }
+}
+
+}
